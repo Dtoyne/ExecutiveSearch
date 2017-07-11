28a5ddb95007d664a28a69042eecc683b4b21e1b
Dtoyne <dylantoyne@gmail.com>
2017-07-11 16:50:57 -0400

    Deploying to Heroku

--- index.js
+++ index.js
@@ -4,8 +4,16 @@ var path = require('path');
 
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.get('/contact', (req, res) => {
+  res.sendFile(path.join(__dirname + '/contact.html'));
+});
+
+app.get('/about', (req, res) => {
+  res.sendFile(path.join(__dirname + '/about.html'));
 });
 
 app.listen(8080);
